f00eed4a8a0fa1717315bbbac05b58c758698791
abdullaharain1309@gmail.com <abdullaharain1309@gmail.com>
2024-06-29 21:34:23 +0500

    added recent project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,19 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import { FloatingNav } from "./components/ui/floating-navbar";
 import Grid from "./components/grid";
+import { FaHome } from "react-icons/fa";
+import RecentProjects from "./components/RecentProjects";
 
 const page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[]}/>
+        <FloatingNav navItems={[
+          {name: 'Home', link: '/', icon: <FaHome/>}
+        ]}/>
         <Hero />
         <Grid/>
+        <RecentProjects/>
       </div>
     </main>
   );
